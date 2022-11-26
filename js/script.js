6ae4a5a7e72097e3b47f16e50a5d6a8486cfa8c2
Artem <artemios69soprun@gmail.com>
2022-11-26 14:36:41 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,99 @@
+
+let canvas = document.querySelector('#canvas')
+let context = canvas.getContext('2d')
+
+let bg = new Image()
+let bird = new Image()
+let fg = new Image()
+let pipeBottom = new Image()
+let pipeUp = new Image()
+
+bg.src = 'img/bg.png'
+bird.src = 'img/bird.png'
+fg.src = 'img/fg.png'
+pipeUp.src = 'img/pipeUp.png'
+pipeBottom.src = 'img/pipeBottom.png'
+
+let audioFly = new Audio()
+let audioScore = new Audio()
+let audioLose = new Audio()
+
+audioFly.src = 'audio/fly.mp3'
+audioScore.src = 'audio/score.mp3'
+audioLose.src = 'audio/lose.mp3'
+
+let gap = 100
+
+let xPos = 10
+let yPos = 150
+let grav = 1.5
+let score = 0
+
+let pipe = []
+
+
+pipe[0] = {
+   x: canvas.width,
+   y: 0
+}
+
+document.addEventListener('keydown', move)
+
+function move(){
+   yPos -= 30
+   audioFly.play()
+
+}
+
+
+function draw(){
+   context.drawImage(bg, 0, 0)
+
+   for (let i = 0; i < pipe.length; i++) {
+      
+      context.drawImage(pipeUp, pipe[i].x, pipe[i].y)
+      context.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap)
+      
+
+      pipe[i].x--
+
+      if(pipe[i].x == 100){
+         pipe.push({
+            x: canvas.width,
+            y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+         })
+      }
+
+      if(yPos + bird.height >= canvas.height - fg.height || yPos  <= bg.height - canvas.height  || xPos + bird.width >= pipe[i].x && xPos + bird.height <= pipe[i].x + pipeUp.width && (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + pipeUp.height + gap)){
+         location.reload()
+         audioLose.play()
+
+      }
+
+      if(pipe[i].x == 5){
+         audioScore.play()
+
+         score++
+      }
+  
+   }
+   
+
+
+
+   context.drawImage(bird, xPos, yPos)
+
+   context.drawImage(fg, 0 , canvas.height - fg.height)
+   
+   yPos += grav
+
+
+   context.fillStyle = '#000'
+   context.font = '24px Verdana'
+   context.fillText(`Score: ${score}`,10, canvas.height - fg.height / 2)
+   
+
+   requestAnimationFrame(draw )
+}
+
+pipeBottom.onload = draw()
